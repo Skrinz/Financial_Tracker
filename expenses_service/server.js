4c35e066285cfbd90c50952f9899f2a864b47a22
Skrinz <111281257+Skrinz@users.noreply.github.com>
2025-07-05 21:47:49 +0800

    Finished API Gateway

--- expenses_service/server.js
+++ expenses_service/server.js
@@ -9,6 +9,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(`[${process.env.SERVICE_NAME}] ${req.method} ${req.url}`);
+  next();
+});
+
 // All /api/expenses routes will require a valid JWT
 app.use("/expenses", validateToken, expenseRoutes);
 
--- user_services/server.js
+++ user_services/server.js
@@ -7,6 +7,10 @@ const userRoutes = require("./routes/userRoutes");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`[${process.env.SERVICE_NAME}] ${req.method} ${req.url}`);
+  next();
+});
 
 app.use("/users", userRoutes);
 
