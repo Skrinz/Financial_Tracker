606d6bc227779ec5967c73b3ed28e1af0486d96d
Skrinz <111281257+Skrinz@users.noreply.github.com>
2025-07-21 13:41:57 +0800

    Removed server folder

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-const expenseRoutes = require("./expenseRoutes");
-const userRoutes = require("./userRoutes");
-const budgetRoutes = require("./budgetRoutes");
-
-module.exports = {
-  expenseRoutes,
-  userRoutes,
-  budgetRoutes
-};
